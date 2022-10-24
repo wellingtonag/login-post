8fa5ee8745ac569cde6add31b543ad2becd9dcd4
Wellington Garcia <42919942+wellingtonag@users.noreply.github.com>
2022-10-24 13:28:11 -0300

    change app file place

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+const router = express.Router();
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.engine('html', require('ejs').renderFile);
+
+app.use('/', router);
+app.listen(process.env.port || 3000) 
+	console.log('Servidor de exemplo aberto na porta: 3000');
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/', 'src', 'pages', 'index.html'));
+});
+
+router.post('/welcome', (req, res) => {
+  res.render(path.join(__dirname, '/', 'src', 'pages', 'welcome.html'), {
+    name: req.body.name,
+  });
+});
+
+module.exports = router;
+
+
+
+
